d3c13926593f940fea0a8b6e92d09d9b55cef281
Angel Gonzalez <58734248+MeBeAngel@users.noreply.github.com>
2022-06-26 18:18:57 -0400

    Workshop 2

---
+++ models/partner.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const partnerSchema = new Schema({
+    name: { 
+        type: String, 
+        unique: true, 
+        required: true 
+    },
+    image: { 
+        type: String, 
+        required: true 
+    },
+    featured: { 
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+})
+
+const Partner = mongoose.model('Partner', partnerSchema);
+module.exports = Partner;
